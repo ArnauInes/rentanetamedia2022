1b31663b62fe7d55815e65e4ee7f65c1a6bd5434
Arnau Inés <144895283+ArnauInes@users.noreply.github.com>
2024-11-07 20:16:33 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js
+
+const mapboxConfig = {
+    accessToken: 'pk.eyJ1IjoiYXJuYXVpbmVzIiwiYSI6ImNsdnFjY3hiMjBkbDQybG1oaDl1MmZxN3IifQ.w9MXQRTssGGK-k7y6OZQSQ',
+    styleURL: 'mapbox://styles/arnauines/clxn69hrc00id01r2c62dfyhr'
+  };
+  
---
+++ config.template.js
@@ -0,0 +1,3 @@
+// config.template.js
+export const MAPBOX_ACCESS_TOKEN = 'YOUR_ACCESS_TOKEN_HERE';
+export const MAPBOX_STYLE_URL = 'YOUR_STYLE_URL_HERE';
